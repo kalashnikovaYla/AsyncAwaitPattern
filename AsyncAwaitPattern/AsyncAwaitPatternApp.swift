aa70b4281f88bd9397a0b4d33881b2ac3d991edc
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-06-24 22:14:13 +0500

    Initial Commit

---
+++ AsyncAwaitPattern/AsyncAwaitPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitPatternApp.swift
+//  AsyncAwaitPattern
+//
+//  Created by sss on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
